5701d0ab13e51107f430a6b0a4d5a6ed8f1cfe37
Sandra Corbacho Alonso <sandracorbacho@gmail.com>
2021-03-25 20:53:59 +0100

    configuracion

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware('auth:api')->group(function(){
+    Route::get('user',[UserController::class,'details']);
+    Route::resource('welcome', WelcomeController::class);
+});
+
+Route::post('login',[UserController::class,'login']);
+Route::post('register', [UserController::class,'register']);
