194a31acddf8a84c177e6af31aac33f26b834fe0
Sandra Corbacho Alonso <sandracorbacho@gmail.com>
2021-04-24 20:03:21 +0200

    error login

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -17,20 +17,16 @@ class CheckRole
      */
     public function handle($request, Closure $next)
     {
-       /* if(empty($request->user()) && empty(\Auth::user())){
-            return redirect()->route('welcome');
-        };*/
-        dd($request->user()->hasRole('admin'));
+       
         if (!empty($request->user()) && ($request->user()->hasRole('admin') || $request->user()->hasRole('loaders') ) ) {
-            
-            return redirect()->route('admin');
+            return $next($request);
         }
         
-        
+       if(!$user->isAdmin()){
             return redirect()->route('welcome');
+        };
         
-        
-        
-        return back()->withErrors(['No estás autorizado']); 
+        return redirect()->route('admin');
+        //return back()->withErrors(['No estás autorizado']); 
     }
 }
